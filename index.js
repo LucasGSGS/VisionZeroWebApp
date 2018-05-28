e94db72ff422b455f42bdfb46d3c9590ff1fb2c5
SHUO GONG <GSGS905480981@gmail.com>
2018-05-28 15:41:46 -0500

    First commit

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 const express = require('express');
+const router = require('express').Router();
 // const bodyParser = require('body-parser');
 const app = express();
 // const _ = require('loadash');
 // const morgan = require('morgan');
 
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
 app.set('port', (process.env.PORT || 4000));
+
+// set the home page route
+app.get("/", function (req, res) {
+ res.send("Hey, I am responding to your request!");
+});
 
 //Start Server
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-// app.get("/", function (req, res) {
-//  res.send("Hey, I am responding to your request!");
-// });
