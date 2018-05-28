f97ccfc3f06554073a4932f1c86d0324ba5ff136
SHUO GONG <GSGS905480981@gmail.com>
2018-05-28 15:17:44 -0500

    First commit

--- index.js
+++ index.js
@@ -7,3 +7,7 @@ const app = express();
 app.listen(3000, function() {
   console.log("App listening on port 3000!");
 });
+
+app.get("/", function (req, res) {
+ res.send("Hey, I am responding to your request!");
+});
