666751f1b266cc6ffa80992eb7855f7cafcc2c58
SHUO GONG <shuogong@wirelessprv-10-195-253-155.near.illinois.edu>
2018-05-28 15:15:29 -0500

    First commit

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+// const bodyParser = require('body-parser');
+const app = express();
+// const _ = require('loadash');
+// const morgan = require('morgan');
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.listen(3000, function() {
+  console.log("App listening on port 3000!");
+});
