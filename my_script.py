ddcf7c5b7f37c48cdcb4443514abfc2c2dc46a67
SHUO GONG <GSGS905480981@gmail.com>
2018-06-01 12:40:41 -0500

    prerun make graph code behine mainpage which can decrease 10s when generating path

--- my_script.py
+++ my_script.py
@@ -9,30 +9,6 @@ import os
 import pandas as pd
 from prerun import Manhattan_Graph
 
-# origin = sys.argv[1].strip().split(",")
-# dest = sys.argv[2].strip().split(",")
-
-# Manhattan_Graph = ox.graph_from_place('Manhattan Island, New York City, New York, USA', network_type='drive')
-# point = (float(y[1]), float(y[0]))
-# nearest_node_id = ox.get_nearest_node(Manhattan_Graph, point)
-# lat = Manhattan_Graph.node[nearest_node_id]['y']
-# lng = Manhattan_Graph.node[nearest_node_id]['x']
-# data = {'id': nearest_node_id, 'latitude': lat, 'longitude': lng}
-# # To write to a file:
-# with open("/Users/shuogong/VisionZeroWebApp/output.json", "w") as f:
-#     json.dump(data, f)
-# print(str(data))
-
-# point = (float(sys.argv[2]), float(sys.argv[1]))
-# print(float(origin[1]), float(origin[0]), float(dest[1]), float(dest[0]))
-
-# To print out the JSON string (which you could then hardcode into the JS)
-# json.dumps(data)
-
-# print('' + lat+','+lng)
-# print('nearest node latlng is '+ '(' + lat + ',' + lng + ')')
-
-
 def penalized_graph(Graph,collision_count,alpha):
     Modified_Graph = Graph.copy()
     for start_node,end_node, edge_data in Modified_Graph.edges(data=True):
